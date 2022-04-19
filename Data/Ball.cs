70260d15dc0a2a2c55c653793a308bb37a094807
Szymon Wrzesiński <236706@edu.p.lodz.pl>
2022-04-19 15:21:26 +0200

    BallsAdded

--- Data/Ball.cs
+++ Data/Ball.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Data
+﻿namespace Data
 {
-    internal class Ball
+    public class Ball
     {
+        private readonly int _ballID;
+        public int XPos { get; set; }
+        public int YPos { get; set; }
+        public int XDirection { get; set; }
+        public int YDirection { get; set; }
+        public int Radius { get; set; }
+        public string Color { get; }
+
+        public Ball(int ID, int xPos, int yPos,int radius, int xDir, int yDir)
+        {
+            _ballID = ID;
+            XPos = xPos;
+            YPos = yPos;
+            XDirection = xDir;
+            YDirection = yDir;
+            Radius = radius;
+            Color = Data.Color.PickColor();
+        }
+
+        public int GetID()
+        {
+            return _ballID;
+        }
     }
 }
