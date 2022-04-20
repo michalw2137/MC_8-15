1bfe6734c3b6f617120338aa14b5844ade449a08
236694 Wielgus Michal <236694@edu.p.lodz.pl>
2022-04-20 15:16:03 +0200

    window for balls

---
+++ Model/Window.cs
@@ -0,0 +1,39 @@
+﻿using Data;
+using LogicLayer;
+
+namespace Presentation.Model
+{
+    public class Window
+    {
+        private readonly BallsManager _ballsManager;
+        //private int _width;
+        //private int _height;
+
+        public void Tick()
+        {
+           _ballsManager.DoTick();
+        }
+
+        public Window(int width, int height)
+        {
+            //_width = width;
+            //_height = height;
+            _ballsManager = new BallsManager(width, height);
+        }
+
+        public List<Ball> GetBalls()
+        {
+            return _ballsManager.GetAllBalls();
+        }
+
+        public void CreateBalls(int amount) 
+        {
+            _ballsManager.SummonBalls(amount);
+        }
+
+        public void ClearWindow()
+        {
+            _ballsManager.ClearMap();
+        }
+    }
+}
