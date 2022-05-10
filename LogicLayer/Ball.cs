5f4c859377275343ac4ac5e42668d441be3db53c
236694 Wielgus Michal <236694@edu.p.lodz.pl>
2022-05-10 15:04:32 +0200

    intuitive ball direction

--- LogicLayer/Ball.cs
+++ LogicLayer/Ball.cs
@@ -20,17 +20,20 @@
             Random rnd = new Random();
             dir = rnd.Next(0, 360);
 
-            speed = rnd.Next(2, 10);
+            speed = 5;
+            //speed = rnd.Next(2, 10);
         }
 
         internal void MoveBallWithinBox(int width, int height)
         {
-            double vx = Math.Cos(dir) * speed;
-            double vy = Math.Sin(dir) * speed;
+            double angle = Math.PI * (dir) / 180.0;
+            double vx = Math.Sin(angle) * speed;
+            double vy = Math.Cos(angle) * speed;
 
             XPosition += (int)vx;
             YPosition += (int)vy;
 
+            BounceIfOnEdge(width, height);
             //int x = 0, y = 0;
             //bool moved = true;
             //while (moved)
@@ -58,20 +61,26 @@
         {
             if (XPosition <= Radius)            // hit left edge, go right
             {
-                XVelocity = Math.Abs(XVelocity);
+                //XVelocity = Math.Abs(XVelocity);
+                dir = 90;
             }
             if (XPosition >= width - Radius)    // hit right edge, go left
             {
-                XVelocity = Math.Abs(XVelocity) * (-1);
+                //XVelocity = Math.Abs(XVelocity) * (-1);
+                dir = -90; 
             }
 
             if (YPosition <= Radius)            // hit bottom edge, go up
             {
-                YVelocity = Math.Abs(YVelocity);
+                //YVelocity = Math.Abs(YVelocity);
+                dir = 0;
             }
             if (YPosition >= height - Radius)   // hit top edge, go down
             {
-                YVelocity = Math.Abs(YVelocity) * (-1);
+                //YVelocity = Math.Abs(YVelocity) * (-1);
+                //dir -= 2 * (dir - 270);
+                dir = 180;
+
             }
         }
 
