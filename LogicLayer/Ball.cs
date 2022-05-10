f70be61601ea6af779ebbebe28a03264c5bab5d0
236694 Wielgus Michal <236694@edu.p.lodz.pl>
2022-05-10 13:52:28 +0200

    changed ball movement to speed and direction

--- LogicLayer/Ball.cs
+++ LogicLayer/Ball.cs
@@ -4,6 +4,8 @@
     {
         private int XVelocity { get; set; }
         private int YVelocity { get; set; }
+        private int dir { get; set; }
+        private float speed { get; set; }
 
         internal Ball(int xPosition, int yPosition, int radius, int xVelocity, int yVelocity)
         {
@@ -14,31 +16,42 @@
             YVelocity = yVelocity;
 
             Radius = radius;
+
+            Random rnd = new Random();
+            dir = rnd.Next(0, 360);
+
+            speed = rnd.Next(2, 10);
         }
 
         internal void MoveBallWithinBox(int width, int height)
         {
-            int x = 0, y = 0;
-            bool moved = true;
-            while (moved)
-            {
-                moved = false;
-                if (x < Math.Abs(XVelocity))
-                {
-                    XPosition += XVelocity / Math.Abs(XVelocity); // -1 if Vel<0, 1 if Vel>0, Vel=0 while will not happen
-                    x++;
-                    moved = true;
+            double vx = Math.Cos(dir) * speed;
+            double vy = Math.Sin(dir) * speed;
 
-                }
-                if (y < Math.Abs(YVelocity))
-                {
-                    YPosition += YVelocity / Math.Abs(YVelocity); // -1 if Vel<0, 1 if Vel>0, Vel=0 while will not happen
-                    y++;
-                    moved = true;
+            XPosition += (int)vx;
+            YPosition += (int)vy;
 
-                }
-                BounceIfOnEdge(width, height);
-            }
+            //int x = 0, y = 0;
+            //bool moved = true;
+            //while (moved)
+            //{
+            //    moved = false;
+            //    if (x < Math.Abs(XVelocity))
+            //    {
+            //        XPosition += XVelocity / Math.Abs(XVelocity); // -1 if Vel<0, 1 if Vel>0, Vel=0 while will not happen
+            //        x++;
+            //        moved = true;
+
+            //    }
+            //    if (y < Math.Abs(YVelocity))
+            //    {
+            //        YPosition += YVelocity / Math.Abs(YVelocity); // -1 if Vel<0, 1 if Vel>0, Vel=0 while will not happen
+            //        y++;
+            //        moved = true;
+
+            //    }
+            //    BounceIfOnEdge(width, height);
+            //}
         }
 
         private void BounceIfOnEdge(int width, int height)
