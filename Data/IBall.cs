431c491efcb366c036b69a69ab92e8682fbf7be9
Szymon Wrzesiński <236706@edu.p.lodz.pl>
2022-05-12 22:56:11 +0200

    minor balls improvements

--- Data/IBall.cs
+++ Data/IBall.cs
@@ -13,11 +13,9 @@ namespace Data
         public int YPosition { get; set; }
         public int Radius { get; set; }
 
-        //public double dir { get; set; }
         public int vx { get; set; }
         public int vy { get; set; }
 
-        //public double speed { get; set; }
         public double mass { get; set; }
 
         public abstract void move();
